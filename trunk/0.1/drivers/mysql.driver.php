941034479e2d3fc56e7d4ad7253d29d722489c76
duanejeffers <duanejeffers@bca40ef1-5444-0410-8bc6-b7499ee18323>
2008-02-06 03:53:36 +0000

    Added support for drivers (see drivers/driverinfo).

---
+++ trunk/0.1/drivers/mysql.driver.php
@@ -0,0 +1,28 @@
+<?php
+	/**
+	 * assetr v0.1 <http://code.google.com/p/assetr/>
+	 *
+	 * Copyright 2008 Duane Jeffers
+	 * This file is part of assetr.
+	 *
+	 * assetr is free software: you can redistribute it and/or modify
+	 * it under the terms of the GNU General Public License as published by
+	 * the Free Software Foundation, either version 3 of the License, or
+	 * (at your option) any later version.
+	 *
+	 * assetr is distributed in the hope that it will be useful,
+	 * but WITHOUT ANY WARRANTY; without even the implied warranty of
+	 * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	 * GNU General Public License for more details.
+	 *
+	 * You should have received a copy of the GNU General Public License
+	 * along with assetr.  If not, see <http://www.gnu.org/licenses/>.
+	 *
+	 * What is this file?:
+	 * mysql.driver.php - This is the mysql driver for assetr. It will be the basis for the other drivers.
+	 **/
+
+	 class mysql {
+		 
+		 
+	 }
